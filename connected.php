c48087cf8c3b5b120f5a4f6e4adfaa73dd298af3
Lusegar <lucas.seggar@epitech.eu>
2023-04-06 17:01:15 +0200

    [ADD] login page

---
+++ connected.php
@@ -0,0 +1,31 @@
+<header>
+    <a href="#"><img src="./asset/imgmail.png" alt="gmail"></a>
+    <nav>
+        <ul>
+            <li><a href="#">POUR LES PROS</a></li>
+            <li><a href="./index.php">CRÉER UN COMPTE</a></li>
+        </ul>
+    </nav>
+</header>
+<body>
+    <main>
+        <div class="form">
+            <h3>Une boîte de réception entièrement repensée</h3>
+            <p>Avec les nouveaux onglets personnalisables, repérez immédiatement les nouveaux messages et choisissez
+                ceux que vous souhaitez lire en priorité.</p>
+            <fieldset id="connection">
+                <legend>Connectez vous</legend>
+                <form method="post">
+                    <label for="email">Mail<sup>*</sup></label> <br>
+                    <input type="text" id="email" name="email" placeholder="Votre mail" aria-required="true"> <br>
+                    <label for="password">Choisir votre mot de passe<sup>*</sup></label> <br>
+                    <input type="password" id="password" name="password" placeholder="Votre mot de passe" aria-required="true"> <br>
+                    <input type="submit" id="submit" value="Connection" aria-label="envoyer">
+                </form>
+            </fieldset>
+        </div>
+        <?php
+            include_once "./src/"
+        ?>
+    </main>
+</body>
