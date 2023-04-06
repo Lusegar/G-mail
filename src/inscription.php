e9c1b583c86ad56d73496cdd9db60d2cbe633aa9
Lusegar <lucas.seggar@epitech.eu>
2023-04-06 16:55:10 +0200

    [MODIF] bdd

--- src/inscription.php
+++ src/inscription.php
@@ -7,20 +7,17 @@ catch (Exception $e){
 }
 if (isset($_POST['email']) || isset($_POST['mdp'])) {
     $_login = $_POST["email"];
-    if (!$_POST['email'] || !$_POST['mdp']) {
-        echo "<p class=\"warning\">Vous avez obliez votre login ou password?</p>";
-        }
-    else if (!filter_var($_login, FILTER_VALIDATE_EMAIL)) {
+    if (!filter_var($_login, FILTER_VALIDATE_EMAIL)) {
             echo "<p class=\"warning\">login invalide</p>";
     }
     else if (is_numeric($_login)) {
         echo "<p class=\"warning\">Vous devez saisir des caractères</p>";
     }
     else {
-        $req = $_bdd->prepare('INSERT INTO inscription (prenom, nom, email, mdp)VALUES(?,?,?,?)');
+        $req = $_bdd->prepare('INSERT INTO connection (prenom, nom, email, mdp)VALUES(?,?,?,?)');
         $req->execute(array($_POST['email'], password_hash($_POST['mdp'], PASSWORD_DEFAULT), $_POST['prenom'], $_POST['nom']));        
         echo "<p class=\"success\">Merci votre contenu est ajouté :
-            <a href=\"connection.inc.php\" title=\"pub\">Connectez vous</a>                
+            <a href=\"../index.php\" title=\"pub\">Connectez vous</a>                
          </p>";
     }
 }
